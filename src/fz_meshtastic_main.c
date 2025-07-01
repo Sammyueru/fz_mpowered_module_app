8f2e85352f898137d43b37d1c8f9bdd79c39bb68
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-07-01 10:37:31 -0600

    init repo

---
+++ src/fz_meshtastic_main.c
@@ -0,0 +1,13 @@
+// Flipper Zero Meshtastic Module App | Copyright 2025, Samm. Licensed under MIT.
+#include <furi.h>
+#include <furi_hal.h>
+#include <gui/gui.h>
+#include <flipper/serial.h>
+
+int32_t fz_meshtastic_main(void* p) {
+    UNUSED(p);
+
+
+
+    return 0;
+}
