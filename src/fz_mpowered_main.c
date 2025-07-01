05ac0820d3d44a2a8916d87dd87e523a3ba7705c
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-07-01 10:46:18 -0600

    refactoring

--- src/fz_mpowered_main.c
+++ src/fz_mpowered_main.c
@@ -1,10 +1,10 @@
-// Flipper Zero Meshtastic Module App | Copyright 2025, Samm. Licensed under MIT.
+// Flipper Zero M-Powered Module App | Copyright 2025, Samm. Licensed under MIT.
 #include <furi.h>
 #include <furi_hal.h>
 #include <gui/gui.h>
 #include <flipper/serial.h>
 
-int32_t fz_meshtastic_main(void* p) {
+int32_t fz_mpowered_main(void* p) {
     UNUSED(p);
 
 
